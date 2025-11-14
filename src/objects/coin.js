171d9aa4b2af61c46623be61e3fafaec56850b45
vseslav-k <vseslavvak@mail.ru>
2025-11-14 12:57:12 -0800

    made coin anim

--- src/objects/coin.js
+++ src/objects/coin.js
@@ -4,12 +4,20 @@ export default class coin extends Phaser.Physics.Arcade.Sprite {
         this.scene = scene;
         scene.add.existing(this);
         scene.physics.add.existing(this);
-        this.body.setSize(8, 8).setOffset(4, 4); // should modify to be ~1 pixel tall and < 1 pixel wide
+        this.body.setSize(8, 8).setOffset(4, 4); 
         this.setDepth(22);    
         this.body.setAllowGravity(false);
         scene.physics.add.collider(player, this, () => {
           this.pickup();
         });
+
+        this.anims.create({
+            key: 'spin',
+            frames: this.anims.generateFrameNumbers('coin', { start: 0, end: 11 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.play('spin');
     }
 
 
